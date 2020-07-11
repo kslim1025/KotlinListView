98ae38c452d6dc0cdde4f2ca29ede9cb14be21d6
choihyunill <kslim1024@gmail.com>
2020-07-11 11:04:11 +0900

    getView

--- app/src/main/java/com/kslim1025/kotlinlistview/adapters/StudentsAdapter.kt
+++ app/src/main/java/com/kslim1025/kotlinlistview/adapters/StudentsAdapter.kt
@@ -4,12 +4,17 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kslim1025.kotlinlistview.R
+import kotlinx.android.synthetic.main.activity_main.view.*
 import org.intellij.lang.annotations.JdkConstants
 
 class StudentsAdapter(val mContext:Context, val resId:Int, val mList:List<String>) {
     val inf = LayoutInflater.from(mContext)
 
-    //override fun getView(position:Int, converView: View?, parent: ViewGroup):ViewGroup{
-        
-    //}
+    override fun getView(position:Int, convertView: View?, parent: ViewGroup):ViewGroup{
+        var tempRow = convertView;
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.studentlist_item,null)
+        }
+    }
 }
