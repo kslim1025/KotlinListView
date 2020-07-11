5b8b9d064cf81292c0b8baaf122fbb60bb4accaf
choihyunill <kslim1024@gmail.com>
2020-07-11 11:00:51 +0900

    Initial commit

---
+++ app/src/main/java/com/kslim1025/kotlinlistview/adapters/StudentsAdapter.kt
@@ -0,0 +1,15 @@
+package com.kslim1025.kotlinlistview.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.intellij.lang.annotations.JdkConstants
+
+class StudentsAdapter(val mContext:Context, val resId:Int, val mList:List<String>) {
+    val inf = LayoutInflater.from(mContext)
+
+    //override fun getView(position:Int, converView: View?, parent: ViewGroup):ViewGroup{
+        
+    //}
+}
---
+++ app/src/main/java/com/kslim1025/kotlinlistview/datas/Students.kt
@@ -0,0 +1,5 @@
+package com.kslim1025.kotlinlistview.datas
+
+class Students(val name:String, val address:String,val birthDay:Int) {
+    
+}
